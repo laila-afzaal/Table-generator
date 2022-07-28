c41bcc93ecad154c40616413615da6add3007cdd
Laila Afzaal <lailaafzaal06@gmail.com>
2022-07-28 17:30:39 +0500

    commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function calculate() {
   let num = document.querySelector('#num').value;
   document.querySelector('#table').innerHTML = ``;
   
-  for (let i = 0; i <= 10; i++) {
+  for (let i = 1; i <= 10; i++) {
       document.querySelector('#table').innerHTML += `<span>${num} x ${i} = ${num *i}<br></span>`;
   }
 } 
